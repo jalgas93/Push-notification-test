3d8559dbd9a9020cc3face118ce0cc7841a06f90
Jalgasbay <ungarbaevjalgas1@gmail.com>
2024-10-16 15:20:50 +0500

    firebase cli success

---
+++ android/app/src/main/kotlin/com/push/notification/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.push.notification
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/DashboardPage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({super.key});
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Push notification test"),
+        centerTitle: true,
+      ),
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Your token:${12344}"),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
